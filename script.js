1bad222d16e7664c96d5e8793473f58ba5a1c3af
mathiaz110 <132710718+mathiaz110@users.noreply.github.com>
2026-03-22 07:37:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 async function registrar(){
 
-let nombre = document.getElementById("nombre").value
-let dni = document.getElementById("dni").value
-let direccion = document.getElementById("direccion").value
+let nombre=document.getElementById("nombre").value
+let dni=document.getElementById("dni").value
+let direccion=document.getElementById("direccion").value
 
-if(nombre=="" || dni=="" || direccion==""){
-alert("Completar todos los campos")
+if(nombre==""||dni==""||direccion==""){
+alert("Completar campos")
 return
 }
 
@@ -15,82 +15,24 @@ await addDoc(collection(db,"usuarios"),{
 nombre,
 dni,
 direccion,
-fecha: new Date()
+fecha:new Date()
 })
 
-alert("Usuario registrado correctamente")
+alert("Registrado correctamente")
 
-}catch(error){
-
-console.error(error)
-alert("Error al guardar")
-
+}catch(e){
+console.error(e)
+alert("Error")
 }
 
 }
 
-
-function actualizarTabla(){
-
-let tabla=document.getElementById("tablaUsuarios")
-
-tabla.innerHTML=""
-
-usuarios.forEach(u=>{
-
-tabla.innerHTML+=`
-
-<tr>
-
-<td>${u.nombre}</td>
-<td>${u.dni}</td>
-<td>${u.direccion}</td>
-
-</tr>
-
-`
-
-})
-
-}
-
-function pagar(){
-
-alert("Aquí se conectará el pago con Mercado Pago")
-
-}
-
-/* modo oscuro */
-
-document.getElementById("themeBtn")
-.addEventListener("click",()=>{
-
-document.body.classList.toggle("light")
-
-})
-
-/* login admin */
-
-let clave=prompt("Panel administrador: ingrese clave")
-
-if(clave=="admin123"){
-
-document.querySelector(".admin").style.display="block"
-
-}
 document.getElementById("adminBtn").addEventListener("click", ()=>{
 
-let clave = prompt("Clave administrador")
+let clave = prompt("Clave admin")
 
 if(clave==="admin123"){
-
-document.querySelector(".admin").style.display="block"
-alert("Modo administrador activado")
-
-}else{
-
-alert("Clave incorrecta")
-
+alert("Admin activado")
 }
 
 })
--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,3 @@
-const CACHE_NAME = "aldia-cache-v1";
-
-const urlsToCache = [
-"/",
-"/index.html",
-"/style.css",
-"/script.js"
-];
-
-self.addEventListener("install", event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => {
-return cache.addAll(urlsToCache);
+self.addEventListener("install", e=>{
+console.log("SW instalado")
 })
-
-);
-
-});
-
-self.addEventListener("fetch", event => {
-
-event.respondWith(
-
-caches.match(event.request)
-.then(response => {
-return response || fetch(event.request);
-})
-
-);
-
-});
