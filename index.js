1c6343c70309b9fa64c2972c6e85ccf35a089a16
mathiaz110 <132710718+mathiaz110@users.noreply.github.com>
2026-03-22 04:58:59 -0300

    Add files via upload
    primera version aldia

---
+++ index.js
@@ -0,0 +1,81 @@
+let usuarios=[]
+
+function scrollRegistro(){
+
+document.getElementById("registro")
+.scrollIntoView({behavior:"smooth"})
+
+}
+
+
+function registrar(){
+
+let nombre=document.getElementById("nombre").value
+let dni=document.getElementById("dni").value
+let direccion=document.getElementById("direccion").value
+
+if(nombre==""||dni==""||direccion==""){
+
+alert("Completar todos los campos")
+return
+
+}
+
+let usuario={nombre,dni,direccion}
+
+usuarios.push(usuario)
+
+actualizarTabla()
+
+alert("Usuario registrado")
+
+}
+
+function actualizarTabla(){
+
+let tabla=document.getElementById("tablaUsuarios")
+
+tabla.innerHTML=""
+
+usuarios.forEach(u=>{
+
+tabla.innerHTML+=`
+
+<tr>
+
+<td>${u.nombre}</td>
+<td>${u.dni}</td>
+<td>${u.direccion}</td>
+
+</tr>
+
+`
+
+})
+
+}
+
+function pagar(){
+
+alert("Aquí se conectará el pago con Mercado Pago")
+
+}
+
+/* modo oscuro */
+
+document.getElementById("themeBtn")
+.addEventListener("click",()=>{
+
+document.body.classList.toggle("light")
+
+})
+
+/* login admin */
+
+let clave=prompt("Panel administrador: ingrese clave")
+
+if(clave=="admin123"){
+
+document.querySelector(".admin").style.display="block"
+
+}
